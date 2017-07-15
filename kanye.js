8647889b95828f56ff51b01bb0f981a60a525397
Gyorgy Muntean <wyatt.g.muntean@gmail.com>
2017-07-15 23:03:14 +0000

    syntax error. needed to comment more shit out.

--- kanye.js
+++ kanye.js
@@ -217,7 +217,7 @@ document.getElementById( "game" ).onclick = function() {
    //grid_button = document.getElementById( "grid" );
    if( ANIMATING ) {
       game_button.value = "Pause";
-      grid_button.style.visibility = 'hidden';
+      //grid_button.style.visibility = 'hidden';
       animate( kanye );
    } else {
       game_button.value = "Play";
